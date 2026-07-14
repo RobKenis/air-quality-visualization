95de34e01b00c5316650d893dc36df2c34f57c87
Rob Kenis <rob.kenis@hotmail.com>
2026-07-14 16:12:18 +0200

    Store location data as json in S3
    Read model for the frontend

--- src/process.py
+++ src/process.py
@@ -1,27 +1,52 @@
 import boto3
 import json
 
-client = boto3.client('s3')
+s3_client = boto3.client("s3")
+dynamodb_client = boto3.client("dynamodb")
 
-BUCKET='openaq-rob-287820185021-eu-west-1-an'
-KEY='processed/locations.json'
+BUCKET = "openaq-rob-287820185021-eu-west-1-an"
+KEY = "processed/locations.json"
+
+
+class Location:
+    def __init__(self, id: str, name: str, longitude: str, latitude: str):
+        self.id = id
+        self.name = name
+        self.longitude = longitude
+        self.latitude = latitude
+
+
+class Measurements:
+    def __init__(self):
+        pass
+
+
+class Statistics:
+    def __init__(self):
+        pass
+
 
 def handler(event, context):
-    for record in event.get('Records', []):
-        locationId = record.get('dynamodb').get('Keys').get('locationId').get('S')
+    for record in event.get("Records", []):
+        locationId = record.get("dynamodb").get("Keys").get("locationId").get("S")
         print(f"New measurement inserted for {locationId}")
 
-        current = client.get_object(
+        location = Location(
+            id=locationId,
+            name=record.get('dynamodb').get('NewImage').get('location').get('S'),
+            longitude=record.get('dynamodb').get('NewImage').get('longitude').get('S'),
+            latitude=record.get('dynamodb').get('NewImage').get('latitude').get('S'),
+        )
+
+        current = s3_client.get_object(
             Bucket=BUCKET,
             Key=KEY,
         )
-        content = current.get('Body').read().decode('utf-8')
+        content = current.get("Body").read().decode("utf-8")
         locations = json.loads(content)
-        
-        locations[locationId] = {}
 
-        client.put_object(
-            Bucket=BUCKET,
-            Key=KEY,
-            Body=json.dumps(locations).encode('utf-8')
+        locations[locationId] = location.__dict__
+
+        s3_client.put_object(
+            Bucket=BUCKET, Key=KEY, Body=json.dumps(locations).encode("utf-8")
         )
